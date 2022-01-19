66d4599c2a0f2120eeb56f5c8b7e624f39f04205
pmutua <pmutua@live.com>
2022-01-19 07:24:57 +0300

    db settings for github workflow

--- pdm/settings/base.py
+++ pdm/settings/base.py
@@ -137,7 +137,7 @@ if POSTGRES_READY:
         }
     }
 
-DATABASES['default'] =  dj_database_url.config()
+    DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
