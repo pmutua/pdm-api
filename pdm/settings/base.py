0b5e8531833479356beffb051ed0c93815e4d5ca
pmutua <pmutua@live.com>
2022-01-19 05:46:49 +0300

    use os.environ for environment variables

--- pdm/settings/base.py
+++ pdm/settings/base.py
@@ -109,14 +109,13 @@ WSGI_APPLICATION = 'pdm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT':config('DB_PORT')
-        
-    } 
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST'),
+        "PORT": os.environ.get('DB_PORT')
+    }
 }
 
 # Password validation
