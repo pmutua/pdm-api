4ecd220f525d194d105fe311d81d72db3ff5b21c
pmutua <pmutua@live.com>
2022-01-19 07:49:36 +0300

    Add settings & github workflow

--- pdm/settings/base.py
+++ pdm/settings/base.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-import dj_database_url
 from decouple import Csv, config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
