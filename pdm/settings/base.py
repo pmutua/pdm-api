1b18434cc3cf7187a9555f2bfe4e7862ad191d8b
pmutua <pmutua@live.com>
2022-01-23 10:22:17 +0300

    chore: Added JWT settings

--- pdm/settings/base.py
+++ pdm/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import datetime
 from pathlib import Path
 
 from decouple import Csv, config
@@ -83,6 +84,47 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+
+# ==============================================================================
+# JWT SETTINGS
+# ==============================================================================
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=36000),
+    'JWT_ENCODE_HANDLER':
+        'api.custom_jwt.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+        'api.custom_jwt.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+        'api.custom_jwt.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+        'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_RESPONSE_PAYLOAD_HANDLER':
+        'api.custom_jwt.jwt_response_payload_handler',
+
+    'JWT_SECRET_KEY': 'secret',
+    'JWT_GET_USER_SECRET_KEY': None,
+    'JWT_PUBLIC_KEY': None,
+    'JWT_PRIVATE_KEY': None,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    # 'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3600),
+    'JWT_AUDIENCE': None,
+    'JWT_ISSUER': None,
+
+    'JWT_ALLOW_REFRESH': False,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+    'JWT_AUTH_COOKIE': None,
+}
 
 # ==============================================================================
 # TEMPLATES SETTINGS
