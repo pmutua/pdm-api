e4c02cfb1beaf87ab5678e4275718cf87505f636
pmutua <pmutua@live.com>
2022-01-24 00:07:41 +0300

    chore: Added testing factories

--- pdm/apps/authentication/factories.py
+++ pdm/apps/authentication/factories.py
@@ -1,7 +1,10 @@
-# factories.py
-from factory.django import DjangoModelFactory
-
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import (
+    Group,
+)
+import factory
+from factory.django import DjangoModelFactory
 
 User = get_user_model()
 
@@ -10,11 +13,17 @@ class UserFactory(DjangoModelFactory):
     class Meta:
         model = "authentication.User"
 
-    username = "harun"
-    password = "staffpassword"
-    email = "harun@email.com"
-    first_name = "Harun"
-    last_name = "Daniels"
+    username = factory.Faker('email')    # password = factory.LazyFunction(lambda: make_password('pi3.1415'))
+    email = factory.Faker('email')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
     dept_id = 1
     phone = "+254722345321"
     identification_no = "23453111"
+
+class GroupFactory(DjangoModelFactory):
+    class Meta:
+        model = Group
+    name = "chief"
+
+
