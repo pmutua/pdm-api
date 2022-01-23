b3cefca7de3656963e6cdeca78c1e17808c902ae
pmutua <pmutua@live.com>
2022-01-24 00:08:07 +0300

    chore: Added new tests add user

--- pdm/apps/authentication/tests.py
+++ pdm/apps/authentication/tests.py
@@ -7,7 +7,10 @@ from pdm.apps.authentication.models import (
 )
 
 from pdm.apps.authentication.serializers import UserDetailSerializer
-from .factories import UserFactory
+from .factories import (
+    UserFactory,
+    GroupFactory
+)
 
 # initialize the APIClient app
 client = APIClient()
@@ -17,9 +20,27 @@ class PostAddNewUserTest(APITestCase):
     """Test module for POST add new user API"""
 
     def setUp(self):
-        self.user = UserFactory
+        self.user = UserFactory.create()
 
-    def test_register_new_user(self):
+    # def test_register_new_user(self):
+    #     url = reverse("register_user")
+    #     data = {
+    #         "first_name": self.user.first_name,
+    #         "last_name": self.user.last_name,
+    #         "email": self.user.email,
+    #         "roles": ["chief", "admin"],
+    #         "phone": self.user.phone,
+    #         "identification_no": self.user.identification_no,
+    #         "department": "Ministry of Development",
+    #     }
+    #     response = client.post(url, data, format="json")
+    #     print(response.data)
+    #     user = User.objects.get(pk=response.json()["data"]["id"])
+    #     serializer = UserDetailSerializer(user)
+    #     self.assertEqual(response.data["data"], serializer.data)
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_user_already_registered(self):
         url = reverse("register_user")
         data = {
             "first_name": self.user.first_name,
@@ -31,10 +52,32 @@ class PostAddNewUserTest(APITestCase):
             "department": "Ministry of Development",
         }
         response = client.post(url, data, format="json")
-        user = User.objects.get(pk=response.json()["data"]["id"])
-        serializer = UserDetailSerializer(user)
-        self.assertEqual(response.data["data"], serializer.data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["data"], None)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class PostLoginUserTest(APITestCase):
+    """Test module for POST add new user API"""
+
+    def setUp(self):
+        self.user = UserFactory.create()
+        self.group = GroupFactory.create()
+        self.user.groups.add(self.group)
+
+    def test_login_user(self):
+        url = reverse("login")
+        data = {
+            "email": self.user.email,
+            "password": self.user.password,
+        }
+
+        response = client.post(url, data, format="json")
+        print(response.data)
+        print(self.user)
+        # user = User.objects.get(pk=response.json()["data"]["id"])
+        # serializer = UserDetailSerializer(user)
+        # self.assertEqual(response.data["data"], serializer.data)
+        # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     # def test_post_invalid_user_payload(self):
     #     pass
+
