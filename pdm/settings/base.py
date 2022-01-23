2bfd5dd8cedb05012d8ef5dac74ee4751e6472a5
pmutua <pmutua@live.com>
2022-01-23 10:23:58 +0300

    chore: Added RESTFRAMEWORK settings

--- pdm/settings/base.py
+++ pdm/settings/base.py
@@ -126,6 +126,33 @@ JWT_AUTH = {
     'JWT_AUTH_COOKIE': None,
 }
 
+
+# ==============================================================================
+# REST FRAMEWORK SETTINGS
+# ==============================================================================
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSESS': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    # Parser classes priority-wise for Swagger
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.JSONParser',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+    'DEFAULT_RENDERER_CLASSESS': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    # 'EXCEPTION_HANDLER': 'authentication.exception_handler.custom_exception_handler'
+}
+
 # ==============================================================================
 # TEMPLATES SETTINGS
 # ==============================================================================
