51e4ca3468627743682ae3bc5d43afe1b924f645
pmutua <pmutua@live.com>
2022-01-26 13:37:15 +0300

    tests: Added factories

--- pdm/apps/demographics/tests/factories.py
+++ pdm/apps/demographics/tests/factories.py
@@ -1,7 +1,16 @@
 
-from factory import Faker
+from factory import (
+    Faker,
+    SubFactory
+)
 from factory.django import DjangoModelFactory
-from  pdm.apps.demographics.models import District
+from  pdm.apps.demographics.models import (
+    District,
+    County,
+    SubCounty,
+    Parish,
+    Village
+)
 
 class DistrictFactory(DjangoModelFactory):
     name = Faker('city')
@@ -9,3 +18,31 @@ class DistrictFactory(DjangoModelFactory):
     class Meta:
         model = District
 
+class CountyFactory(DjangoModelFactory):
+    name = Faker('city')
+    district = SubFactory(DistrictFactory)
+
+    class Meta:
+        model = County
+
+class SubCountyFactory(DjangoModelFactory):
+    name = Faker('city')
+    county = SubFactory(CountyFactory)
+
+    class Meta:
+        model = SubCounty
+
+class ParishFactory(DjangoModelFactory):
+    name = Faker('city')
+    sub_county = SubFactory(SubCountyFactory)
+
+    class Meta:
+        model = Parish
+
+
+class VillageFactory(DjangoModelFactory):
+    name = Faker('city')
+    parish = SubFactory(ParishFactory)
+
+    class Meta:
+        model = Village
