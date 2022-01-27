37795e8032194f6fc2cdafef5b4726de23ec6bc5
samdetrix <ochiengsamuel38@gmail.com>
2022-01-27 17:30:54 +0300

    sam:changes on API

--- pdm/apps/production_storage_processing_marketing/views.py
+++ pdm/apps/production_storage_processing_marketing/views.py
@@ -1,9 +1,33 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from pdm.apps.farmers.models import Farmer
 from pdm.apps.production_storage_processing_marketing.models import Evoucher
 from pdm.apps.production_storage_processing_marketing.serializers import EvoucherSerializer
 
 
-class EvouchersAPIView(ListCreateAPIView):
-    serializer_class = EvoucherSerializer
-    queryset = Evoucher.objects.all()
+class EvouchersAPIView(APIView):
+    def get(self, request):
+        evouchers = Evoucher.objects.all()
+        evouchers_serializer = EvoucherSerializer(evouchers, many=True)
+        res = {}
+        res["data"] = evouchers_serializer.data
+        res["success"] = True
+
+        return Response(res)
+
+    def post(self, request):
+        request_data = request.data
+        beneficiary = request_data.get("beneficiary")
+        voucher_no = request_data.get("voucher_no")
+        value = request_data.get("value")
+        farm_input = request_data.get("farm_input")
+        farmer = Farmer.objects.get(id=beneficiary)
+        evoucher = Evoucher.objects.create(beneficiary=farmer,
+                                           voucher_no=voucher_no,
+                                           value=value,
+                                           farm_input=farm_input)
+        evouchers_serializer = EvoucherSerializer(evoucher)
+
+        res = {"success": True, "evoucher": evouchers_serializer.data}
+        return Response(res)
