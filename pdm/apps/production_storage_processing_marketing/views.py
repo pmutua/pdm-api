7edfe5b1a6cabf34f000a130bc9fd183861324b3
pmutua <pmutua@live.com>
2022-01-31 23:54:35 +0300

    update view and url

--- pdm/apps/production_storage_processing_marketing/views.py
+++ pdm/apps/production_storage_processing_marketing/views.py
@@ -3,10 +3,16 @@ from django.db.models import Sum
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView
-
-from pdm.apps.farmers.models import Farmer
+from rest_framework.views import APIView
+from pdm.apps.farmers.models import (
+    Farmer,
+    Produce
+)
 from pdm.apps.demographics.models import District, County
-from pdm.apps.production_storage_processing_marketing.models import Evoucher, PSPMPillarProgram
+from pdm.apps.production_storage_processing_marketing.models import (
+    Evoucher,
+    PSPMPillarProgram
+)
 from pdm.apps.production_storage_processing_marketing.serializers import (
     EvoucherSerializer,
     EvoucherCreateSerializer,
@@ -54,55 +60,125 @@ class DashBoardApiView(ListCreateAPIView):
         districts_data = {
             "total_evouchers_issued": Evoucher.objects.all().aggregate(Sum("value"))["value__sum"] or 0,
             "no_of_farmers_registered": Farmer.objects.all().count(),
+            "total_income_value_from_processing": Produce.objects.filter(produce_in__iexact='sold').aggregate(Sum("value"))["value__sum"] or 0,
+            "total_value_of_produce_in_storage": Produce.objects.filter(produce_in__iexact='storage').aggregate(Sum("value"))["value__sum"] or 0,
             "districts": [
                 {
+                    "id":district.id,
                     "name": district.name,
                     "counties": [
                         {
+                            "id": county.id,
                             "name": county.name,
                             "sub_counties": [
                                 {
+                                    "id": subcounty.id,
                                     "name": subcounty.name,
                                     "parishes": [
                                         {
+                                            "id": parish.id,
                                             "name": parish.name,
                                             "villages": [
                                                 {
+                                                    "id": village.id,
                                                     "name": village.name,
                                                     "total": Evoucher.objects.filter(
                                                         beneficiary__village__id=village.id
                                                     ).aggregate(Sum("value"))["value__sum"]
-                                                    or 0,
+                                                             or 0,
+                                                    "income_processing_value": Produce.objects.filter(
+                                                        produce_in__iexact='sold',
+                                                        owner__village__id=village.id
+                                                    ).aggregate(Sum("value"))["value__sum"]
+                                                                               or 0,
+                                                    "total_value_of_produce_in_storage":
+                                                        Produce.objects.filter(produce_in__iexact='storage',
+                                                                               owner__village__id=village.id).aggregate(
+                                                            Sum("value"))[
+                                                            "value__sum"] or 0,
                                                 }
                                                 for village in parish.villages.all()
                                             ],
                                             "total": Evoucher.objects.filter(
                                                 beneficiary__village__parish__id=parish.id
                                             ).aggregate(Sum("value"))["value__sum"]
-                                            or 0,
+                                                     or 0,
+                                            "income_processing_value": Produce.objects.filter(
+                                                produce_in__iexact='sold',
+                                                owner__village__parish__id=parish.id
+                                            ).aggregate(Sum("value"))["value__sum"]
+                                                                       or 0,
+                                            "total_value_of_produce_in_storage":
+                                                Produce.objects.filter(produce_in__iexact='storage',
+                                                                       owner__village__parish__id=parish.id).aggregate(
+                                                    Sum("value"))[
+                                                    "value__sum"] or 0,
                                         }
                                         for parish in subcounty.parishes.all()
                                     ],
                                     "total": Evoucher.objects.filter(
                                         beneficiary__village__parish__sub_county__id=subcounty.id
                                     ).aggregate(Sum("value"))["value__sum"]
-                                    or 0,
+                                             or 0,
+                                    "income_processing_value": Produce.objects.filter(
+                                        produce_in__iexact='sold',
+                                        owner__village__parish__sub_county__id=subcounty.id
+                                    ).aggregate(Sum("value"))["value__sum"]
+                                                               or 0,
+                                    "total_value_of_produce_in_storage":
+                                        Produce.objects.filter(produce_in__iexact='storage',
+                                                               owner__village__parish__sub_county__id=subcounty.id).aggregate(
+                                            Sum("value"))[
+                                            "value__sum"] or 0,
                                 }
                                 for subcounty in county.sub_counties.all()
                             ],
                             "total": Evoucher.objects.filter(
                                 beneficiary__village__parish__sub_county__county__id=county.id
                             ).aggregate(Sum("value"))["value__sum"]
-                            or 0,
+                                     or 0,
+                            "income_processing_value": Produce.objects.filter(
+                                produce_in__iexact='sold',
+                                owner__village__parish__sub_county__county__id=county.id
+                            ).aggregate(Sum("value"))["value__sum"]
+                                                       or 0,
+                            "total_value_of_produce_in_storage":
+                                Produce.objects.filter(produce_in__iexact='storage',owner__village__parish__sub_county__county__id=county.id).aggregate(Sum("value"))[
+                                    "value__sum"] or 0,
                         }
                         for county in district.counties.all()
                     ],
-                    "total_value": Evoucher.objects.filter(
+                    "total": Evoucher.objects.filter(
                         beneficiary__village__parish__sub_county__county__district__id=district.id
                     ).aggregate(Sum("value"))["value__sum"]
-                    or 0,
+                                   or 0,
+                    "income_processing_value": Produce.objects.filter(
+                        produce_in__iexact='sold',
+                        owner__village__parish__sub_county__county__district__id=district.id
+                    ).aggregate(Sum("value"))["value__sum"]
+                                               or 0,
+                    "total_value_of_produce_in_storage":
+                        Produce.objects.filter(produce_in__iexact='storage',
+                                               owner__village__parish__sub_county__county__district__id=district.id).aggregate(
+                            Sum("value"))[
+                            "value__sum"] or 0,
+                    "total_farmers_registered":
+                        Farmer.objects.filter(village__parish__sub_county__county__district__id=district.id).count(),
                 }
                 for district in District.objects.all()
             ],
         }
         return Response(data=json.dumps(districts_data, cls=DecimalEncoder))
+
+
+class SetUpPillarAPIView(APIView):
+    def post(self,request):
+        data = request.data
+        commencement_date = data.get('date')
+        funds_disbursed = data.get('funds_disbursed')
+        obj = PSPMPillarProgram.objects.create(
+            commencement_date=commencement_date,
+            funds_disbursed=funds_disbursed
+        )
+
+
