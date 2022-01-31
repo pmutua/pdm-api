840ac8ecc3579e90ef8626c495168db107787c5a
pmutua <pmutua@live.com>
2022-01-31 12:06:18 +0300

    chore: Added utility to encode decimal values

--- pdm/apps/production_storage_processing_marketing/utilities.py
+++ pdm/apps/production_storage_processing_marketing/utilities.py
@@ -1,12 +1,21 @@
+import json
+import decimal
 from pdm.apps.production_storage_processing_marketing.models import Evoucher
-
 
 
 def autogenerate_evoucher_no():
     """Autogenerate E-voucher no"""
     last_obj = Evoucher.objects.all().last()
     if last_obj is None:
-        return 'ev-0'
+        return "ev-0"
     else:
-        return f'ev-{last_obj.id+1}'
+        return f"ev-{last_obj.id+1}"
 
+
+class DecimalEncoder(json.JSONEncoder):
+    """Encodes Decimal objects"""
+
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super(DecimalEncoder, self).default(o)
