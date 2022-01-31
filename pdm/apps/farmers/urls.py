1c2e228498a8fa072f99f1e6ed245b97854ebeaa
pmutua <pmutua@live.com>
2022-01-31 14:16:11 +0300

    farmers: Added new endpoint.

--- pdm/apps/farmers/urls.py
+++ pdm/apps/farmers/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from pdm.apps.farmers.views import (
     FarmersAPIView,
     RegisterFarmerAPIView,
-    CropAPIView
+    CropAPIView,
+    RecordProduceAPIview
 )
 
 urlpatterns = [
     path('', FarmersAPIView.as_view(), name="farmers"),
     path('register_farmer', RegisterFarmerAPIView.as_view(), name="register-farmer"),
     path('crops', CropAPIView.as_view(), name="crops"),
+    path('produce/add', CropAPIView.as_view(), name="add-produce"),
 ]
